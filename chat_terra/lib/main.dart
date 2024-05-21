fe657c947dc841e449e0d71d99639d0a1a1a1b50
GemmaGarrigosa <gemma.garrigosa@gmail.com>
2024-05-21 20:09:13 +0200

    Afegint dependencies firebase

--- chat_terra/lib/main.dart
+++ chat_terra/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:chat_terra/views/chatpage.dart';
 import 'package:chat_terra/views/loginpage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions
+        .currentPlatform, //mira en quina plataforma s'està inicialitzant
+  );
   runApp(const MyApp());
 }
 
